e0fd82f204b192765f0d5e2208a4a010420e8da3
TheOwlCoder <100481826+TheOwlCoder@users.noreply.github.com>
2022-10-04 19:36:33 -0500

    gfc

--- expsnap/src/threads.js
+++ expsnap/src/threads.js
@@ -3205,7 +3205,7 @@ Process.prototype.doGlideTo = function (secs, name) {
 
     if (thisObj) {
         if (this.inputOption(name) === 'center') {
-            thisObj.glide(secs, 0, 0);
+            glide(secs, 0, 0);
         } else if (this.inputOption(name) === 'mouse-pointer') {
             thisObj.glide(secs, this.reportMouseX(), this.reportMouseY());
         } else if (this.inputOption(name) === 'random position') {
